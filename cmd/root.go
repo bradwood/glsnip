147cc04e710a77fe9db8487de5c995fa07d837b9
Bradley Wood <git@bradleywood.com>
2020-11-23 17:23:01 +0000

    bump version and tidy up code comments

--- cmd/root.go
+++ cmd/root.go
@@ -2,18 +2,17 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
 
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Version: "0.0.2",
+	Version: "0.0.3",
 	Use:     "glsnip",
 	Short:   "Copy and Paste using GitLab Snippets",
 	Long: `This app behaves like pbcopy(1) and pbpaste(1) on a Mac, or like xclip(1) on
@@ -32,8 +31,6 @@ Environment variables:
   all alphabetic characters to UPPERCASE.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -46,20 +43,16 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default $HOME/.glsnip)")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".glsnip" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".glsnip")
 		viper.SetConfigType("yaml")
@@ -68,12 +61,6 @@ func initConfig() {
 	}
 
 	viper.SetEnvPrefix("glsnip")
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
+	viper.AutomaticEnv()
 	viper.ReadInConfig()
-
-	// if err := viper.ReadInConfig(); err == nil {
-	//	fmt.Println("Using config file:", viper.ConfigFileUsed())
-	// }
 }
